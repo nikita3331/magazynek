84b5d0fb4c363d6161114b7f79787055fd99530c
nikita3331 <40238256+nikita3331@users.noreply.github.com>
2020-03-12 18:08:59 +0100

    wdw
    dwwd

--- zapytania/zapytania.py
+++ zapytania/zapytania.py
@@ -10,7 +10,7 @@ from PyQt5.QtGui import QPalette
 import json
 global ex
 
-#dodawanie pracownika ,wyszukiwanie z tabelki z filtrowaniem
+#generujemy obiegowke ,mail idzie do mamy z tabelki wybieramy pracownika i sprawdzamy jeszcze czy nic na nim nie ma
 
 url='https://zapkaappka.herokuapp.com/'
 #url='http://127.0.0.1:5000/'
